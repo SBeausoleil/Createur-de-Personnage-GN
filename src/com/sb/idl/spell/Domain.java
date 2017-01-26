22313e8d1cce4897e78b648ebe9e8d60ef9eae4f
Samuel Beausoleil <samuel.beausoleil@hotmail.com>
2017-01-26 12:39:46 -0500

    Added constructors and getter/setter

--- src/com/sb/idl/spell/Domain.java
+++ src/com/sb/idl/spell/Domain.java
@@ -5,4 +5,47 @@ import java.util.Set;
 public class Domain<SpellType extends Spell> {
     private String name;
     private Set<SpellType> spells;
+
+    public Domain(String name, Set<SpellType> spells) {
+	this.name = name;
+	this.spells = spells;
+    }
+
+    /**
+     * Returns the name.
+     * 
+     * @return the name
+     */
+    public String getName() {
+	return name;
+    }
+
+    /**
+     * Sets the value of name to that of the parameter.
+     * 
+     * @param name
+     *            the name to set
+     */
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    /**
+     * Returns the spells.
+     * 
+     * @return the spells
+     */
+    public Set<SpellType> getSpells() {
+	return spells;
+    }
+
+    /**
+     * Sets the value of spells to that of the parameter.
+     * 
+     * @param spells
+     *            the spells to set
+     */
+    public void setSpells(Set<SpellType> spells) {
+	this.spells = spells;
+    }
 }
--- src/com/sb/idl/spell/Spell.java
+++ src/com/sb/idl/spell/Spell.java
@@ -3,9 +3,157 @@ package com.sb.idl.spell;
 public class Spell<SpellType extends Spell> {
     private String name;
     private long duration;
+    /**
+     * The time required to cast the spell.
+     * It is either the number of words to be said (mage) or the length of the prayer (priest).
+     */
     private long castingTime;
     private String description;
     private float range;
     private int cost;
     private Domain<SpellType> domain;
+
+    public Spell(String name, long duration, long castingTime, String description, float range, int cost,
+	    Domain<SpellType> domain) {
+	this.name = name;
+	this.duration = duration;
+	this.castingTime = castingTime;
+	this.description = description;
+	this.range = range;
+	this.cost = cost;
+	this.domain = domain;
+    }
+
+    /**
+     * Returns the name.
+     * 
+     * @return the name
+     */
+    public String getName() {
+	return name;
+    }
+
+    /**
+     * Sets the value of name to that of the parameter.
+     * 
+     * @param name
+     *            the name to set
+     */
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    /**
+     * Returns the duration.
+     * 
+     * @return the duration
+     */
+    public long getDuration() {
+	return duration;
+    }
+
+    /**
+     * Sets the value of duration to that of the parameter.
+     * 
+     * @param duration
+     *            the duration to set
+     */
+    public void setDuration(long duration) {
+	this.duration = duration;
+    }
+
+    /**
+     * Returns the castingTime.
+     * 
+     * @return the castingTime
+     */
+    public long getCastingTime() {
+	return castingTime;
+    }
+
+    /**
+     * Sets the value of castingTime to that of the parameter.
+     * 
+     * @param castingTime
+     *            the castingTime to set
+     */
+    public void setCastingTime(long castingTime) {
+	this.castingTime = castingTime;
+    }
+
+    /**
+     * Returns the description.
+     * 
+     * @return the description
+     */
+    public String getDescription() {
+	return description;
+    }
+
+    /**
+     * Sets the value of description to that of the parameter.
+     * 
+     * @param description
+     *            the description to set
+     */
+    public void setDescription(String description) {
+	this.description = description;
+    }
+
+    /**
+     * Returns the range.
+     * 
+     * @return the range
+     */
+    public float getRange() {
+	return range;
+    }
+
+    /**
+     * Sets the value of range to that of the parameter.
+     * 
+     * @param range
+     *            the range to set
+     */
+    public void setRange(float range) {
+	this.range = range;
+    }
+
+    /**
+     * Returns the cost.
+     * 
+     * @return the cost
+     */
+    public int getCost() {
+	return cost;
+    }
+
+    /**
+     * Sets the value of cost to that of the parameter.
+     * 
+     * @param cost
+     *            the cost to set
+     */
+    public void setCost(int cost) {
+	this.cost = cost;
+    }
+
+    /**
+     * Returns the domain.
+     * 
+     * @return the domain
+     */
+    public Domain<SpellType> getDomain() {
+	return domain;
+    }
+
+    /**
+     * Sets the value of domain to that of the parameter.
+     * 
+     * @param domain
+     *            the domain to set
+     */
+    public void setDomain(Domain<SpellType> domain) {
+	this.domain = domain;
+    }
 }
