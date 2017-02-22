b1c6c3daee9509ea004a90fb1875f4925e269545
Samuel Beausoleil <samuel.beausoleil@hotmail.com>
2017-02-22 17:01:27 -0500

    Created package for the specific data pertaining to the LARPG.

---
+++ src/com/sb/cdp/idl/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package serves to contain all data pertaining to the initialization of the CDP system for the Ile Des Legendes LARPG.
+ */
+/**
+ * 
+ * @author Samuel Beausoleil
+ *
+ */
+package com.sb.cdp.idl;
